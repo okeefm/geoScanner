cbfb49e45f56e0bfb8717df9e4acd5a76d82637f
Michael O'Keefe <okeefm@rpi.edu>
2011-05-03 15:00:11 -0400

    Got the intent between the two activities to work

--- gen/com/okeefm/geoscanner/R.java
+++ gen/com/okeefm/geoscanner/R.java
@@ -16,10 +16,14 @@ public final class R {
     public static final class id {
         public static final int barcodeEditText=0x7f050001;
         public static final int button1=0x7f050002;
-        public static final int button2=0x7f05000b;
+        public static final int button2=0x7f05000c;
+        public static final int cancelButton=0x7f05000d;
         public static final int dateEditText=0x7f050006;
         public static final int labelEditText=0x7f05000a;
+        public static final int linearLayout1=0x7f05000b;
         public static final int locationEditText=0x7f050004;
+        public static final int newBarcodeButton=0x7f05000e;
+        public static final int tableLayout1=0x7f05000f;
         public static final int textView1=0x7f050000;
         public static final int textView2=0x7f050009;
         public static final int textView3=0x7f050003;
@@ -28,15 +32,18 @@ public final class R {
         public static final int timeEditText=0x7f050008;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int add=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int barcodeScanFail=0x7f040005;
+        public static final int cancelButtonText=0x7f04000c;
         public static final int dateEditText=0x7f040006;
         public static final int dateTextView=0x7f040007;
         public static final int hello=0x7f040000;
         public static final int locationTextViewName=0x7f040004;
+        public static final int newBarcodeButton=0x7f04000d;
         public static final int saveButton=0x7f040003;
         public static final int scanButtonName=0x7f040002;
         public static final int setDateButtonText=0x7f04000a;
